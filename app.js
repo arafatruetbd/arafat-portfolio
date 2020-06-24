27fde83a272d265982481d9bb293f399c36b8780
arafatruetbd <63414511+arafatruetbd@users.noreply.github.com>
2020-06-25 02:56:32 +0600

    Init Commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
